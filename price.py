0ae5b4475d42d2d99309a83efcee26addf686a45
tjwls0502 <insui13188208@gmail.com>
2022-06-08 23:47:10 +0900

    first commit

--- price.py
+++ price.py
@@ -1,22 +1,3 @@
-from pathlib import Path
-from urllib.request import urlretrieve
-
-base_url = "https://raw.githubusercontent.com/codingalzi/pybook/master/jupyter-book/data/"
-
-data_path = Path() / "data"
-data_path.mkdir(parents=True, exist_ok=True)
-
-def myWget(filename):
-    # 다운로드 대상 파일 경로
-    file_url = base_url + filename
-
-    # 저장 경로와 파일명
-    target_path = data_path / filename
-
-    return urlretrieve(file_url, target_path)
-
-myWget("shopA.txt")
-
 def shopping(shop_file):
     
     with open('data/shopA.txt', mode='r', encoding='utf-8') as f:
@@ -31,7 +12,7 @@ def shopping(shop_file):
     return shop_dict
 
 def item_price(shop_file, item):
-    
+    ...
     with open('data/shopA.txt', mode='r', encoding='utf-8') as f:
       lines = f.readlines()
       shop_dict = {}
